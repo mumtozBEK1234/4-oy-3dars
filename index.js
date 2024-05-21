7ad6c943a199d470570c975ce131792df37ed6fc
mumtozBEK1234 <email manzilingiz>
2024-05-21 20:34:40 +0300

    4-oy 3 dars

---
+++ index.js
@@ -0,0 +1,94 @@
+// 1 - masala
+// let son = +prompt("sonni kiritin")
+// if (son > 0) {
+//     console.log('$ {son} son musbat');
+// } else {
+//     console.log('$ {son} son musbat emas');
+// }
+
+// // 2-masala
+// let son = +prompt("sonni kiritin")
+// if (son % 2) {
+//     console.log('$ {son} toqson');
+// } else {
+//     console.log('$ {son} toqson emas');
+// }
+
+// 3-masala
+// let a = +prompt("a sonni kiritin")
+// if (a % 2) {
+//     console.log('${a} a sonni juft emas');
+   
+// } else{
+//     console.log('${a} a sonni juft');
+// }
+
+// 4-masla
+// let a = + prompt("a sonni kiritin")
+// let b = + prompt("b sonni kiritin")
+// if (a > 2 || b <= 3) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 5-masala
+// let a = + prompt("a sonni kiritin")
+// let b = + prompt("b sonni kiritin")
+// if(a >= 0 || b <-2) {
+// console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 6-masala
+
+
+// 7-masla
+
+// 8-masala
+// let a = +prompt("a sonni kiritin")
+// let b = +prompt("b sonni kiritin")
+// if (a % 2 || b % 2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// // 9-masal
+//     let a = +prompt("a sonni kiritin")
+//     let b = +prompt("b sonni kiritin")
+//     if (a % 2 || b % 2) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+
+// 15-masala 
+// let a = +prompt("a sonni kiritin")
+// let b = +prompt("b sonni kiritin")
+// let c = +prompt("c sonni kiritin")
+// if(a >= 0 &&  b >= 0) {
+//     console.log(true);
+// }
+// if(b >= 0 &&  c >= 0) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 16-masla
+// let a = +prompt("a sonni kiritin")
+// if ( a > 9 && a < 100 && a % 2 == 0) {
+//     console.log(true);
+// } else{
+//     console.log(false);
+
+// }
+
+
+   
+
+
+
